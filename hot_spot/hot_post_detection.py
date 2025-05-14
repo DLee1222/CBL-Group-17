6770f2f651a600127f0c1e35784523f2888b1afc
Roman <r.cenalmor.roca@student.tue.nl>
2025-05-14 11:12:07 +0200

    Adding Hot-Spot z-score

--- hot_spot/hot_post_detection.py
+++ hot_spot/hot_post_detection.py
@@ -1,11 +1,19 @@
 import pandas as pd
-import os
 
 
-data = "../data_processed/burglary_lsoa_month_2010_2025.csv"
-df = pd.read_csv(data)
+df = pd.read_csv("hot-spot.csv")
+total_crime = df["Burglary_Count"].sum()
 
-df_burglary_aggregated = df.groupby("LSOA code", as_index = False)['Burglary_Count'].sum()
-output_path = "hot-spot"
-df_burglary_aggregated.to_csv(output_path, index=False)
-print(f"Saved full burglary dataset to {output_path}")
+df_sorted = df.sort_values("Burglary_Count", ascending=False).reset_index(drop=True)
+df_sorted["Cumulative"] = df_sorted["Burglary_Count"].cumsum()
+df_sorted["Cumulative_Percent"] = df_sorted["Cumulative"] / total_crime * 100
+
+threshold_index = df_sorted[df_sorted["Cumulative_Percent"] >= 50].index[0]
+num_lsoas_50pct = threshold_index + 1
+
+print(f"Total burglary count: {total_crime}")
+print(f"Number of LSOAs accounting for 50% of total crime: {num_lsoas_50pct}")
+
+top_lsoas = df_sorted.head(num_lsoas_50pct)
+print("\nLSOAs contributing to the first 50% of crime:")
+print(top_lsoas[["LSOA code", "Burglary_Count", "Cumulative_Percent"]].to_string(index=False))
