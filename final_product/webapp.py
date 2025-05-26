8d56c8893c6e621181948eb1f9bc81a37302d556
Roman <r.cenalmor.roca@student.tue.nl>
2025-05-26 13:04:07 +0200

    Added the search bar functionality

--- final_product/webapp.py
+++ final_product/webapp.py
@@ -4,7 +4,8 @@ import plotly.express as px
 from dash import Dash, html, dcc
 import dash_leaflet as dl
 from dash.dependencies import Input, Output
-from mercantile import feature
+import plotly.graph_objects as go
+from soupsieve.util import lower
 
 app = Dash(__name__)
 
@@ -17,7 +18,7 @@ burglary_df = pd.read_csv('burglary.csv')
 #Create a dictionary such that the key is the lsoa_code and the value is a dictionary of the information of that LSOA.
 data_lookup = burglary_df.set_index('LSOA code').to_dict(orient='index')
 
-#create the heatmap
+
 heatmap = px.choropleth_map(
     burglary_df,
     geojson=lsoa_geojson,
@@ -25,7 +26,7 @@ heatmap = px.choropleth_map(
     featureidkey='properties.LSOA21CD',
     color='Burglary_Count',
     color_continuous_scale="viridis",
-    range_color = (10, 350),
+    range_color = (burglary_df['Burglary_Count'].quantile(0.05), burglary_df['Burglary_Count'].quantile(0.95)),
     map_style="white-bg",
     zoom=8.85,
     opacity=0.7,
@@ -33,79 +34,142 @@ heatmap = px.choropleth_map(
     labels={'Burglary_Count': 'Burglary Count'},
 )
 
-app.layout = app.layout = html.Div(
-
+app.layout = html.Div(
     style={
         "display": "flex",
         "padding": "20px",
     },
     children=[
         html.Div(
-            style={"display": "flex", "flexDirection": "column", "gap": "10px", },
+            style={'display': 'flex', 'flexDirection': 'row', 'gap': '10px'},
             children=[
-                html.H1("LSOA Burglary Heatmap", style={"margin": "0"}),
 
-            html.Div(
-            style= {'display': 'flex', 'flexDirection': 'row', 'gap': '10px'},
-            children =[
                 html.Div(
-                    style={'display': 'flex', 'flexDirection': 'column', 'gap': '10px'},
-                    # column for stacking vertically
+                    style={
+                        "position": "relative",
+                        "width": "800px",
+                        "height": "600px",
+                        "backgroundColor": "white",
+                        "boxShadow": "0 0 10px rgba(0,0,0,0.1)",
+                        "borderRadius": "8px",
+                        "overflow": "hidden",
+                    },
                     children=[
-                        html.Div(
+                        dcc.Graph(
+                            id="map",
+                            figure=heatmap,
+                            style={"width": "100%", "height": "100%"},
+                            config={"displayModeBar": False},
+                        ),
+                        html.H1(
+                            "LSOA Burglary Heatmap",
                             style={
-                                "display": "flex",
-                                "flexDirection": "row",
-                                "width": "800px",
-                                "height": "600px",
-                                "backgroundColor": "white",
-                                "boxShadow": "0 0 10px rgba(0,0,0,0.1)",
-                                "borderRadius": "8px",
-                                "overflow": "hidden",
+                                "position": "absolute",
+                                "top": "10px",
+                                "left": "50%",
+                                "transform": "translateX(-50%)",
+                                "margin": 0,
+                                "pointerEvents": "none",
+                                "color": "rgba(0,0,0,0,0.8)",
+                                "fontSize": "1.75rem",
                             },
-                            children=[
-
-                                html.Div(
-                                    style={"flex": "1 1 auto"},
-                                    children=[
-                                        dcc.Graph(
-                                            id="map",
-                                            figure=heatmap,
-                                            style={"height": "100%", "width": "100%"},
-                                            config={"displayModeBar": False,},
-                                            )
-                                        ],
-                                    ),
-                            ],
                         ),
                     ],
                 ),
-                html.Div(style={"display": "flex", "flexDirection": "column", "gap": "10px",
-                                "width": "200px", "height": "600px", "justifyContent": "flex-start"},
-                         children=[
-                             html.Label("Search LSOA Code:"),
-                             dcc.Input(
-                                 id='lsoa-input',
-                                 type='text',
-                                 placeholder='Enter LSOA code',
-                                 debounce=True,
-                                 style={"width": "100%", "padding": "8px"}
-                             ),
-                             html.Div(id='search-feedback', style={"color": "red"})
-                         ],
+
+                html.Div(
+                    style={
+                        "display": "flex",
+                        "flexDirection": "column",
+                        "gap": "10px",
+                        "width": "200px",
+                        "height": "600px",
+                        "justifyContent": "flex-start",
+                    },
+                    children=[
+                        html.Label("Search LSOA Code:"),
+                        dcc.Input(
+                            id='lsoa-input',
+                            type='text',
+                            placeholder='Enter LSOA code',
+                            debounce=True,
+                            style={"width": "100%", "padding": "8px"}
+                        ),
+                        html.Div(id='search-feedback', style={"color": "red"})
+                    ],
                 ),
             ],
-         )
-
-            ],
-    ),
-
-    ],
+        )
+    ]
 )
 
+@app.callback(
+    Output('map', 'figure'),
+    Output('search-feedback', 'children'),
+    Input('lsoa-input', 'value')
+)
+def zoom_to_lsoa(code):
+    if not code:
+        return heatmap, ""
 
+    code = code.strip().upper()
 
+    if code not in data_lookup:
+        return heatmap, "LSOA code not found."
 
+    polygon = None
+    for f in lsoa_geojson['features']:
+        if f['properties']['LSOA21CD'] == code:
+            polygon = f
+            break
+    if polygon is None:
+        return heatmap, f"No geometry polygon for" f"LSOA code {code}."
+
+    coordinates = []
+
+    def gather_coordinates(coordinate_list):
+        for coordinate in coordinate_list:
+            if isinstance(coordinate[0], list):
+                gather_coordinates(coordinate)
+            else:
+                coordinates.append(coordinate)
+
+    gather_coordinates(polygon['geometry']['coordinates'])
+
+    longitude = [pt[0] for pt in coordinates]
+    latitude = [pt[1] for pt in coordinates]
+    center = {"lon": sum(longitude) / len(longitude), "lat": sum(latitude) / len(latitude)} #computing the average to get the center
+
+    updated_heatmap = px.choropleth_map(
+        burglary_df,
+        geojson=lsoa_geojson,
+        locations='LSOA code',
+        featureidkey='properties.LSOA21CD',
+        color='Burglary_Count',
+        color_continuous_scale="viridis",
+        range_color = (burglary_df['Burglary_Count'].quantile(0.05), burglary_df['Burglary_Count'].quantile(0.95)),
+        map_style="white-bg",
+        zoom=12,
+        center=center,
+        opacity=0.7,
+        labels={'Burglary_Count': 'Burglary Count'},
+    )
+
+    lons, lats = zip(*coordinates)
+    updated_heatmap.add_trace(go.Scattermap(
+        lon=lons + (lons[0],),
+        lat=lats + (lats[0],),
+        mode='lines',
+        line=dict(width=3, color='red'),
+        fill=None,
+        hoverinfo='skip',
+        showlegend=False
+    ))
+
+    count = data_lookup[code]['Burglary_Count']
+    feedback = f"LSOA {code} recorded {count} burglaries."
+
+    return updated_heatmap, feedback
 
 
 if __name__ == '__main__':
